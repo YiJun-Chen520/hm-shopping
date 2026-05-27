0b166f9d1154514d38f45c7c8ae253156f556f57
ChenYijun <3098517519@qq.com>
2026-05-27 17:35:29 +0800

    添加 Axios 依赖，更新样式文件，新增多个图片资源，实现登录页面的图形验证码功能

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,7 +1,8 @@
 // vant-ui组件库
 import Vue from 'vue'
-import { Button, Tabbar, TabbarItem } from 'vant'
+import { Button, Tabbar, TabbarItem, NavBar } from 'vant'
 
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Button)
+Vue.use(NavBar)
