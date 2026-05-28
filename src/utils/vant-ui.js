f8099794c52c2d83d450c9fa898af4792150f495
ChenYijun <3098517519@qq.com>
2026-05-28 22:08:36 +0800

    新增首页组件，整合首页数据获取及展示逻辑，优化页面布局和样式

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,9 +1,14 @@
 // vant-ui组件库
 import Vue from 'vue'
-import { Button, Tabbar, TabbarItem, NavBar, Toast } from 'vant'
+import { Button, Tabbar, TabbarItem, NavBar, Toast, Search, Swipe, SwipeItem, Grid, GridItem } from 'vant'
 
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Button)
 Vue.use(NavBar)
 Vue.use(Toast)
+Vue.use(GridItem)
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
